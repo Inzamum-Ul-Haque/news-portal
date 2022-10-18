61bfac1a95b5049cdfb16374fdcf8129cde3708d
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-19 00:48:24 +0600

    right and left nav stylings, calling data from API

--- src/components/Shared/RightNav/RightNav.js
+++ src/components/Shared/RightNav/RightNav.js
@@ -1,9 +1,55 @@
 import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { Button, ButtonGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaYoutube,
+  FaWhatsapp,
+  FaTwitter,
+  FaDiscord,
+  FaRegStickyNote,
+  FaRegListAlt,
+} from "react-icons/fa";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>This is right nav</h2>
+      <ButtonGroup vertical>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button className="mb-2" variant="outline-dark">
+          <FaGithub /> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-4">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaYoutube /> Youtube
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> WhatsApp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaDiscord /> Discord
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaRegStickyNote /> Privacy Policy
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaRegListAlt /> Terms & Conditions
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
